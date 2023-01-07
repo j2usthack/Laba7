3d5b43974c949b583a2e2a331039851458a6ee1a
j2usthack <dimas.mihailov@gmail.com>
2023-01-07 17:04:00 +0700

    Добавил $

--- MathTaskClassLibrary/Work4.cs
+++ MathTaskClassLibrary/Work4.cs
@@ -4,7 +4,7 @@ namespace MathTaskClassLibrary
 {
     public class Work4
     {
-        private readonly Regex _regex = new Regex(@"^(\w+\.)?\w+@(\w+\.){1,2}\w{1,4}");
+        private readonly Regex _regex = new Regex(@"^(\w+\.)?\w+@(\w+\.){1,2}\w{1,4}$");
         public bool EmailCheck(string email) => _regex.IsMatch(email);
     }
 }
